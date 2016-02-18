51b63aa5b815b0879be8f6eddc35bd3d2ea2227d
xxdeyy <xxdeyy@gmail.com>
2016-02-18 15:44:24 +0800

    没啥特别的

---
+++ php/hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>犯二？重命名？</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v=1" />
+</head>
+<body>
+
+<?php
+
+echo strpos("Hello world!", "wor");
+
+?>
+
+</body>
+</html>
